396b34d378e8afb2511357572316a9f879e75eb9
Edson Belchior <contatoedsongama@hotmail.com>
2024-11-07 15:42:25 -0300

    fix: correcao de bug na barra de rolagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './styles/inicio.css';
-import './styles/como-funciona.css'
-import './styles/recursos.css'
+import './styles/como-funciona.css';
+import './styles/recursos.css';
 
 import { Button } from './components/ui/button';
 
@@ -12,33 +12,55 @@ import pecaAbout from './assets/peca.png';
 import cabecalho from './assets/cabecalho.png';
 
 function Navbar() {
+  const smoothScroll = (event, targetId) => {
+    event.preventDefault();
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <nav style={styles.navbar}>
       <ul style={styles.navList}>
-        <li><a href="inicio" style={styles.navItem}>Início</a></li>
-        <li><a href="como funciona" style={styles.navItem}>Como Funciona</a></li>
-        <li><a href="recursos" style={styles.navItem}>Recursos</a></li>
-        <li><a href="contato" style={styles.navItem}>Contato</a></li>
+        <li>
+          <a href="#inicio" style={styles.navItem} onClick={(e) => smoothScroll(e, 'inicio')}>
+            Início
+          </a>
+        </li>
+        <li>
+          <a href="#about" style={styles.navItem} onClick={(e) => smoothScroll(e, 'about')}>
+            Como Funciona
+          </a>
+        </li>
+        <li>
+          <a href="#recursos" style={styles.navItem} onClick={(e) => smoothScroll(e, 'recursos')}>
+            Recursos
+          </a>
+        </li>
+        <li>
+          <a href="#contato" style={styles.navItem} onClick={(e) => smoothScroll(e, 'contato')}>
+            Contato
+          </a>
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
 
 function App() {
   return (
     <>
-      {/* Barra de navegação. */}
+      {/* Barra de navegação */}
       <Navbar />
       <div className='slogan'>
         <img src={imageSlogan} alt="Slogan" className="Slogan" />
       </div>
 
-      {/* Sessão: início. */}
+      {/* Sessão: início */}
       <section className="inicio" id="inicio">
-
         <div className='desc'>
-          <h1 id="titulo">Sua Plataforma de Apoio e Conexão
-            para Famílias e Especialistas</h1>
+          <h1 id="titulo">Sua Plataforma de Apoio e Conexão para Famílias e Especialistas</h1>
           <p id="sub-titulo">
             Aqui, você é capaz de encontrar o apoio que <br />
             precisa para cuidar de quem você ama.
@@ -48,7 +70,6 @@ function App() {
             <Button className="rounded-sm" size="lg" variant="outline">Sou uma clínica</Button>
           </div>
         </div>
-
         <div className="image-container">
           <img src={imageElipse} alt="Elipse" className="background-image" />
           <img src={imageBebe} alt="Child" className="overlay-image" />
@@ -56,7 +77,7 @@ function App() {
       </section>
 
       {/* Sessão: Sobre Nós */}
-      <section className='about'>
+      <section className='about' id='about'>
         <div className='sectionAbout'>
           <h2 id='title'>Como Funciona?</h2>
           <p id='subtitle'>O Connect Azul conecta famílias com profissionais<br />
@@ -70,39 +91,33 @@ function App() {
         <div className='imageAbout'>
           <img src={imageAbout} alt="image" className="aboutImage" />
         </div>
-
         <div className="Puzzle">
           <img src={pecaAbout} alt="quebra-cabeça" className="puzzleImage" />
         </div>
-
       </section>
 
       {/* Sessão: Recursos */}
-      <section className='recursos'>
+      <section className='recursos' id='recursos'>
         <div className='cabecalho'>
           <img src={cabecalho} alt="cabeçalho" className="cabecalhoImage" />
         </div>
-
-        <div class="resources-section">
+        <div className="resources-section">
           <h2>Recursos</h2>
-
-          <div class="resources-container">
-            <div class="resource-column">
+          <div className="resources-container">
+            <div className="resource-column">
               <h3>Fonoaudiologia no Autismo:</h3>
               <ul>
                 <li><a href="#link1">A Importância da Fonoaudiologia no Autismo - Autismo em Foco</a></li>
                 <li><a href="#link2">Fonoaudiologia para crianças com autismo – Ipa Therapy</a></li>
               </ul>
             </div>
-
-            <div class="resource-column">
+            <div className="resource-column">
               <h3>Terapia Ocupacional:</h3>
               <ul>
                 <li><a href="#link3">Terapia Ocupacional e Autismo - Viva e Deixe Viver</a></li>
               </ul>
             </div>
-
-            <div class="resource-column">
+            <div className="resource-column">
               <h3>Musicoterapia no Autismo:</h3>
               <ul>
                 <li><a href="#link4">Musicoterapia no Tratamento do Autismo</a></li>
@@ -111,27 +126,19 @@ function App() {
             </div>
           </div>
         </div>
-
-
       </section>
-
     </>
   );
 }
-
-
-
 
 const styles = {
   navbar: {
     padding: '1em',
     textAlign: 'left',
-    position: 'fixed',
     top: 0,
     width: '100%',
     zIndex: 1000,
   },
-
   navList: {
     listStyle: 'none',
     padding: 0,
@@ -146,22 +153,6 @@ const styles = {
   section: {
     padding: '2em',
     textAlign: 'center',
-  },
-};
-
-const home = {
-  container: {
-    position: 'relative',
-    width: '300px', // ajuste conforme necessário
-    height: '300px', // ajuste conforme necessário
-  },
-  image: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    objectFit: 'cover', // mantém a proporção da imagem
   },
 };
 
