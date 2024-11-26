8496e373ecbf03a10b4b9e5b1fc31a50bc2216d8
Arthur Holanda <holandaarthur46@gmail.com>
2024-11-26 11:23:06 -0300

    removendo axios

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
 
 import "../styles/login.css";
 import imageSlogan from "../assets/Slogan.png";
